1a0b2ef567a50414e084efb326aa7364c274db83
thanhlm <thanhlm@welearning.vn>
2024-07-09 17:50:25 +0700

    9/7/2024

--- RnCourse/App.js
+++ RnCourse/App.js
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [value, onChangeText] = useState('Useless Multiline Placeholder');
   return (
-    <View style={styles.container}>
-      <Text>Hello world</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} value={value} placeholder='Your Goal' />
+        <Button title='Add' />
+      </View>
+      <View style={styles.goalsContainer}>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    paddingTop: 50,
+    paddingBottom: 20,
+    paddingHorizontal: 20,
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
   },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    
+  },
+  input: {
+    borderColor: 'black',
+    borderWidth: 1,
+    width: '80%',
+    padding: 10
+  },
+  goalsContainer :{
+    
+  }
+
 });
